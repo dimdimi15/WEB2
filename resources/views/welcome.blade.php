83e8fa3e4d775e1f7f96792dac776bddf27ebfb6
Dimas Anggara saputra <149036833+dimdimi15@users.noreply.github.com>
2024-06-03 19:38:21 +0700

    Add files via upload

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WELCOME</title>
+</head>
+
+<body>
+    <h1>WELCOME GAISE</h1>
+    <p>selamat datang</p>
+
+    <a href="{{route('posts.index')}}">
+        <button>View All posts</button>
+    </a>
+
+
+</body>
+
+</html>
